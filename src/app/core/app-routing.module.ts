01fdde2da48bfef4b7609eeea380086a4a448f72
Humphrey Mugambi <mugagambi@gmail.com>
2017-04-01 00:14:07 +0300

    initial commit

---
+++ src/app/core/app-routing.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { AuthGuard } from './auth.gaurd';
+import { HomeComponent } from '../pages/home/home.component';
+import { CreateComponent } from '../pages/create/create.component';
+import { CompleteComponent } from '../pages/complete/complete.component';
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot([
+      {
+        path: '',
+        component: HomeComponent
+      },
+      {
+        path: 'create',
+        component: CreateComponent,
+        canActivate: [
+          AuthGuard
+        ]
+      },
+      {
+        path: 'complete',
+        component: CompleteComponent,
+        canActivate: [
+          AuthGuard
+        ]
+      },
+      {
+        path: '**',
+        redirectTo: '',
+        pathMatch: 'full'
+      }
+    ])
+  ],
+  providers: [AuthGuard],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRoutingModule { }
